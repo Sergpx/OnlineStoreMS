8f730c3fad38882e6812f33b0809133782208102
Sergp <sergp>
2024-03-28 21:16:25 +0300

    create storage structure

---
+++ storage/src/main/java/com/example/storage/models/Supply.java
@@ -0,0 +1,18 @@
+package com.example.storage.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "suppliers")
+public class Supply {
+}
